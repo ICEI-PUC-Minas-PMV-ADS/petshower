26612f681d215d8344690d382dbf48361aa9e8c6
Kayki Letieri <kaykiletieri37@gmail.com>
2023-10-11 09:42:08 -0300

    feat(auth): add auth

--- src/ProjetoPet/Domain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetoPet.Domain.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
