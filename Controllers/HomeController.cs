2d34199cbf86e162cb5794d2643b84803845ff91
Kayki Letieri <kaykiletieri37@gmail.com>
2023-10-18 10:31:19 -0300

    fix(using): fix using blazor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PetShower.Models;
+using PetShower.Helpers;
 
 namespace PetShower.Controllers;
 
@@ -26,6 +26,6 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorView { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
